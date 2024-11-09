fa5a78e4c7b0e3a229e6024a8e7f0ada97e54d4a
Sahin Meric <sahin.meric@medisprout.com>
2024-11-08 23:15:41 -0500

    Created simple home page component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Welcome to OrderFlow</h1>
+      <p>Manage your products and orders efficiently.</p>
+    </div>
+  );
+};
+
+export default Home;
