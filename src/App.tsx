2dfe0a6dca2d3990e5049d165f6ff23730e606c3
Sahin Meric <sahin.meric@medisprout.com>
2024-11-08 23:16:53 -0500

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
