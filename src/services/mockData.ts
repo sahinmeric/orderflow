e79083996d3618ba389a0a6817824923822d04c1
Sahin Meric <sahin.meric@medisprout.com>
2024-11-10 07:42:19 -0500

    mock product data added,  mock order data will be added

---
+++ src/services/mockData.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Product A",
+    description: "Description of Product A",
+    price: 100,
+    stock: 10,
+  },
+  {
+    id: 2,
+    name: "Product B",
+    description: "Description of Product B",
+    price: 200,
+    stock: 5,
+  },
+  {
+    id: 3,
+    name: "Product C",
+    description: "Description of Product C",
+    price: 150,
+    stock: 8,
+  },
+];
