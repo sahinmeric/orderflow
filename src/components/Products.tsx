be589875e3f8b43c54b0f54b74df57720b1f6ef0
Sahin Meric <sahin.meric@medisprout.com>
2024-11-10 08:32:26 -0500

    added Add product button and Modal

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,20 +1,51 @@
-import React from "react";
-import { Box, CircularProgress } from "@mui/material";
-import useGetProducts from "../hooks/useGetProducts";
+import React, { useEffect, useState } from "react";
+import { Container, Button, Stack } from "@mui/material";
+import AddProductModal from "./AddProductModal";
 import ProductCard from "./ProductCard";
+import useGetProducts from "../hooks/useGetProducts";
+import { Product } from "../types/types";
 
 const Products = () => {
   const { data: products, loading, error } = useGetProducts();
+  const [openModal, setOpenModal] = useState(false);
+  const [productList, setProductList] = useState<Product[]>([]);
 
-  if (loading) return <CircularProgress />;
-  if (error) return <p>{error}</p>;
+  useEffect(() => {
+    setProductList(products);
+  }, [products]);
+
+  const handleAddProduct = (newProduct: {
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+  }) => {
+    const uniqueId = `${Date.now()}-${Math.random()
+      .toString(36)
+      .substring(2, 9)}`;
+    const productWithId: Product = { ...newProduct, id: uniqueId };
+    setProductList([...productList, productWithId]);
+  };
 
   return (
-    <Box p={3}>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </Box>
+    <Container maxWidth="md">
+      <Stack direction="row" justifyContent="flex-start" mb={2}>
+        <Button variant="contained" onClick={() => setOpenModal(true)}>
+          Add Product
+        </Button>
+      </Stack>
+      {error && <p>{error}</p>}
+      {!loading &&
+        productList.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+
+      <AddProductModal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        onSave={handleAddProduct}
+      />
+    </Container>
   );
 };
 
