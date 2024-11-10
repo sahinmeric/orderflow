4ee9ba81146627a5b9b1511236e8b310cad5fbba
Sahin Meric <sahin.meric@medisprout.com>
2024-11-10 08:41:15 -0500

    updated edit button funtionality

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,34 +4,21 @@ import { Product } from "../types/types";
 
 type ProductCardProps = {
   product: Product;
+  onEdit: (product: Product) => void;
 };
 
-const ProductCard = ({ product }: ProductCardProps) => {
+const ProductCard = ({ product, onEdit }: ProductCardProps) => {
   return (
     <Container
       maxWidth="md"
-      sx={{
-        border: "1px solid #ddd",
-        borderRadius: "8px",
-        padding: "16px",
-        marginBottom: "16px",
-      }}
+      sx={{ border: "1px solid #ddd", padding: "16px", marginBottom: "16px" }}
     >
       <Typography variant="h6">{product.name}</Typography>
-      <Typography
-        variant="body2"
-        color="text.secondary"
-        sx={{ marginBottom: "8px" }}
-      >
-        {product.description}
-      </Typography>
+      <Typography variant="body2">{product.description}</Typography>
       <Typography variant="body1">Price: ${product.price}</Typography>
-      <Typography variant="body1" sx={{ marginBottom: "16px" }}>
-        Stock: {product.stock}
-      </Typography>
-
+      <Typography variant="body1">Stock: {product.stock}</Typography>
       <Stack direction="row" spacing={2} justifyContent="flex-end">
-        <Button variant="contained" color="primary">
+        <Button variant="contained" onClick={() => onEdit(product)}>
           Edit
         </Button>
         <Button variant="outlined" color="error">
